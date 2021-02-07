536a2d4915d9256fc040a9c44d3617605e3b711b
Samarvir singh <samarvir1996@gmail.com>
2021-02-07 17:58:31 +0100

    Add: prettier script - yarn run format

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,19 @@
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBnGUVsD4dqkI6-eemZlIXVIlp56YDWWT4",
-    authDomain: "nomegle.firebaseapp.com",
-    projectId: "nomegle",
-    storageBucket: "nomegle.appspot.com",
-    messagingSenderId: "679800466085",
-    appId: "1:679800466085:web:3047e6bac97acad42abb4c",
-    measurementId: "G-MEXJETYK40"
+  apiKey: 'AIzaSyBnGUVsD4dqkI6-eemZlIXVIlp56YDWWT4',
+  authDomain: 'nomegle.firebaseapp.com',
+  projectId: 'nomegle',
+  storageBucket: 'nomegle.appspot.com',
+  messagingSenderId: '679800466085',
+  appId: '1:679800466085:web:3047e6bac97acad42abb4c',
+  measurementId: 'G-MEXJETYK40'
 };
-  
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export const analytics = firebase.analytics();
