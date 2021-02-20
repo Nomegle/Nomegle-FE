990b8aa37123f8ca10e8e9c9bd338727ceb99029
Thorben <bender.thorben0@gmail.com>
2021-02-20 15:27:43 +0100

    Create Prototype page

---
+++ src/pages/prototype/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Prototype = () : JSX.Element => {
+    return (
+        <div>
+            <p>Prototype</p>
+        </div>
+    )
+}
+
+export default Prototype;
