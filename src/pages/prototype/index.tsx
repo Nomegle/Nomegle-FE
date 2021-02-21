399d8b70174c76930feda529c5ddea7bb052834f
Thorben <bender.thorben0@gmail.com>
2021-02-21 15:12:29 +0100

    Add: twillio-video

--- src/pages/prototype/index.tsx
+++ src/pages/prototype/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect, createLocalTracks } from 'twilio-video';
 
 
 const Prototype = () : JSX.Element => {
+
+    useEffect(() => {
+        console.log('Log')
+    }, [])
+
     return (
         <div>
             <p>Prototype</p>
+            <div className="myVideo">
+            </div>
+            <div className="otherVideo">
+            </div>
         </div>
     )
 }
