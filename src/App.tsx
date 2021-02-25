5f7625c45c93f1cb873c14d02271041105061e6f
Thorben <bender.thorben0@gmail.com>
2021-02-25 23:26:58 +0100

    Work on prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { analytics } from './config';
 import { Route } from 'react-router-dom';
 import Homepage from './pages/homepage';
-import Prototype from './pages/prototype'
+import Prototype from './pages/prototype';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/prototype" component={Prototype} />
+      <Route exact path="/prototype/:user" component={Prototype} />
       {analytics.logEvent('Visited Homepage')}
     </div>
   );
--- src/pages/prototype/index.tsx
+++ src/pages/prototype/index.tsx
@@ -1,19 +1,107 @@
-import React, { useEffect } from 'react';
-import { connect, createLocalTracks } from 'twilio-video';
+import React, { useEffect, useState } from 'react';
+// import { connect, createLocalTracks } from 'twilio-video';
+import axios from 'axios'
+import { createLocalTracks, createLocalVideoTrack } from 'twilio-video';
+import { connect } from 'twilio-video';
+import { isPropertySignature } from 'typescript';
 
 
-const Prototype = () : JSX.Element => {
+const Prototype = (props: any) : JSX.Element => {
+    const backendURL = 'https://us-central1-nomegle.cloudfunctions.net/widgets';
+
+    const [token, setToken] = useState('')
+    const [roomName, setRoomName] = useState('')
+    const [error, setError] = useState('')
+    const [room, setRoom] = useState<any>(undefined)
+
+
+    async function getToken(roomName: string) : Promise<void> {
+        try {
+            console.log('running')
+            const res = await axios.post(`${backendURL}/accessToken`, {room: roomName, sid: '', user: props.match.params.user});
+            const { data } = res;
+            if (data) {
+                setToken(data)
+            } else {
+                setError('Token does not exist')
+            }
+        } catch (err) {
+            console.log(err)
+            setError(err.message)
+        }
+    }
+
+    function setLocalVideo() {
+        const myVideo : any = document.getElementById('myVideo');
+        if (myVideo.children.length === 0) {
+            createLocalVideoTrack().then(track => {
+                if (myVideo) {
+                    myVideo.appendChild(track.attach())
+                }
+            })
+        }
+    }
+
+    function isPageLoaded() {
+        return document.getElementById('page') !== null ? true : false
+    }
+
+    function createRoom(token : string) : void {
+        createLocalTracks({
+            audio: true,
+            video: { width: 640 }
+        }).then(localtracks => {
+            setLocalVideo()
+            return connect(token, {
+                name: roomName,
+                tracks: localtracks
+            }).then(room => {
+                setRoom(room)
+                room.on('participantConnected', participant => {
+                    const otherVideo : HTMLElement | null = document.getElementById('otherVideo')
+                    const name = document.createElement('p')
+                    name.innerHTML = participant.identity;
+                    otherVideo?.appendChild(name)
+                    participant.tracks.forEach(publication => {
+                        if (publication.isSubscribed) {
+                          const track : any = publication.track;
+                          const otherVideo : any = document.getElementById('otherVideo');
+                          otherVideo.appendChild(track.attach());
+                        }
+                      });
+                    
+                      participant.on('trackSubscribed', (track : any) => {
+                        const otherVideo : any = document.getElementById('otherVideo')
+                        if (otherVideo.children.length === 0) {
+                            otherVideo.appendChild(track.attach());
+                        }
+                      });
+                    });
+                })
+            })
+    }
 
     useEffect(() => {
-        console.log('Log')
+        setRoomName('gaming-time')
     }, [])
 
+    useEffect(() => {
+        if (roomName && isPageLoaded()) getToken(roomName);
+    }, [roomName, isPageLoaded])
+
+
+    useEffect(() => {
+        if (token) {
+            createRoom(token)
+        }
+    }, [token])
+
     return (
-        <div>
+        <div id="page">
             <p>Prototype</p>
-            <div className="myVideo">
+            <div id="myVideo">
             </div>
-            <div className="otherVideo">
+            <div id="otherVideo">
             </div>
         </div>
     )
